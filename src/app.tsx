da8fe06ed41082d9e7cbd880a60c6f5d9191f981
Wilson Franca <wilson.franca.92@gmail.com>
2025-07-08 16:06:01 -0700

    feat: configure react-query

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,19 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { CreateRoomPage } from './pages/create-room'
 import { RoomPage } from './pages/room'
 
+const queryClient = new QueryClient()
+
 export function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route element={<CreateRoomPage />} index />
-        <Route element={<RoomPage />} path="/room" />
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<CreateRoomPage />} index />
+          <Route element={<RoomPage />} path="/room" />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   )
 }
