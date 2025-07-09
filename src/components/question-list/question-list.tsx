a67f519d2864b3189fefe9a4c8378cb1ce3a2a69
Wilson Franca <wilson.franca.92@gmail.com>
2025-07-09 16:02:33 -0700

    feat/refactor(room): move questions list to a separate component and add useRoomQuestions hook for the api call

---
+++ src/components/question-list/question-list.tsx
@@ -0,0 +1,24 @@
+import { useRoomQuestions } from '@/http/use-room-questions'
+import { QuestionItem } from '../question-item'
+
+interface QuestionListProps {
+  roomId: string
+}
+
+export const QuestionList = ({ roomId }: QuestionListProps) => {
+  const { data } = useRoomQuestions(roomId)
+
+  return (
+    <div className="space-y-6">
+      <div className="flex items-center justify-between">
+        <h2 className="font-semibold text-2xl text-foreground">Q&A</h2>
+      </div>
+
+      {
+        !data?.map((question) => (
+          <QuestionItem key={question.id} question={question} />
+        ))
+      }
+    </div>
+  )
+}
