6ddc8f09567cb4c776a423267fb9a01934526189
Wilson Franca <wilson.franca.92@gmail.com>
2025-07-10 14:47:03 -0700

    feat(pages): add record audio room page

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { CreateRoomPage } from './pages/create-room'
+import { RecordRoomAudioPage } from './pages/record-room-audio'
 import { RoomPage } from './pages/room'
 
 const queryClient = new QueryClient()
@@ -12,6 +13,7 @@ export function App() {
         <Routes>
           <Route element={<CreateRoomPage />} index />
           <Route element={<RoomPage />} path="/room/:roomId" />
+          <Route element={<RecordRoomAudioPage />} path="/room/:roomId/audio" />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
