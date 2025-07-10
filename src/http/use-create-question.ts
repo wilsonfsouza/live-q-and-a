91538c6c8ee62adfa8bc3f4e7489978931de0e7e
Wilson Franca <wilson.franca.92@gmail.com>
2025-07-10 14:45:07 -0700

    refactor(question-item): apply optimistic interface to show question when the user first enters it and change behavior while backend api is processing

--- src/http/use-create-question.ts
+++ src/http/use-create-question.ts
@@ -1,6 +1,7 @@
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import type { CreateQuestionRequest } from './types/create-question-request'
 import type { CreateQuestionResponse } from './types/create-question-response'
+import type { GetRoomQuestionsResponse } from './types/get-room-questions-response'
 
 export function useCreateQuestion(roomId: string) {
   const queryClient = useQueryClient()
@@ -24,8 +25,67 @@ export function useCreateQuestion(roomId: string) {
 
       return result
     },
-    onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['get-rooms', roomId] })
+    // When API call happens
+    onMutate: ({ question }) => {
+      const questions = queryClient.getQueryData<GetRoomQuestionsResponse>([
+        'get-questions',
+        roomId,
+      ])
+
+      const questionsArray = questions ?? []
+
+      const newQuestion = {
+        id: crypto.randomUUID(),
+        question,
+        answer: null,
+        createdAt: new Date().toISOString(),
+        isGeneratingAnswer: true,
+      }
+
+      queryClient.setQueryData<GetRoomQuestionsResponse>(
+        ['get-questions', roomId],
+        [newQuestion, ...questionsArray]
+      )
+
+      return {
+        newQuestion,
+        questions,
+      }
+    },
+    onError: (_error, _variables, context) => {
+      if (context?.questions) {
+        queryClient.setQueryData<GetRoomQuestionsResponse>(
+          ['get-questions', roomId],
+          context.questions
+        )
+      }
+    },
+    onSuccess: (data, _variables, context) => {
+      queryClient.setQueryData<GetRoomQuestionsResponse>(
+        ['get-questions', roomId],
+        (questions) => {
+          if (!questions) {
+            return questions
+          }
+
+          if (!context?.newQuestion) {
+            return questions
+          }
+
+          return questions.map((question) => {
+            if (question.id === context.newQuestion.id) {
+              return {
+                ...context.newQuestion,
+                id: data.questionId,
+                answer: data.answer,
+                isGeneratingAnswer: false,
+              }
+            }
+
+            return question
+          })
+        }
+      )
     },
   })
 }
