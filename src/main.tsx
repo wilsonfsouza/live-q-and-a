c8088e233dfbf4dce1312c98a07b115e823aa25e
Wilson Franca <wilson.franca.92@gmail.com>
2025-07-16 12:18:07 -0700

    feat: change base theme and move global.css file inside styles folder

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { App } from './app.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./app.tsx";
+import "./styles/global.css";
 
 // biome-ignore lint/style/noNonNullAssertion: mandatory by React
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
   </StrictMode>
-)
+);
