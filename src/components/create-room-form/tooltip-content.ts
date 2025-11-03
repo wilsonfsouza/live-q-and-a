bac0707e0124b8af7225887d105f57f866f9dc3d
Wilson Franca <wilson.franca.92@gmail.com>
2025-11-03 12:41:22 -0800

    feat: add form floating tooltip and modal for room creation UX

---
+++ src/components/create-room-form/tooltip-content.ts
@@ -0,0 +1,20 @@
+export const tooltipContent = {
+  name: {
+    heading: "Writing a room name",
+    items: [
+      "Name must be simple with concise information.",
+      "It can be formulated as a question, but that's not a requirement.",
+      "Example1: What are react hooks?",
+      "Example2: Introduction to Personal Finance",
+    ],
+  },
+  description: {
+    heading: "Writing a room description",
+    items: [
+      "This field is optional.",
+      "Use this field to include additional information, such as the room's goals, rules, and resources.",
+      "You can use this field to share links and resources with your audience.",
+      "The content here can help your audience make better questions.",
+    ],
+  },
+};
