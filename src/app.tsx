12b455be0d63dc063962409f164a228d33e58ddb
Wilson Franca <wilson.franca.92@gmail.com>
2025-11-03 10:55:00 -0800

    refactor: replace page to record an audio with a custom hook

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,9 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { CreateRoomPage } from './pages/create-room'
-import { RecordRoomAudioPage } from './pages/record-room-audio'
-import { RoomPage } from './pages/room'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { CreateRoomPage } from "./pages/create-room";
+import { RoomPage } from "./pages/room";
 
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 export function App() {
   return (
@@ -13,9 +12,8 @@ export function App() {
         <Routes>
           <Route element={<CreateRoomPage />} index />
           <Route element={<RoomPage />} path="/room/:roomId" />
-          <Route element={<RecordRoomAudioPage />} path="/room/:roomId/audio" />
         </Routes>
       </BrowserRouter>
     </QueryClientProvider>
-  )
+  );
 }
